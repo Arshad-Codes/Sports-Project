b96578635e66de2bd0db7c717b1b7f2cb96bdd5d
Nifras365 <nifrasmjm56@outlook.com>
2023-12-29 01:37:42 +0530

    Update
    New Data's added
    React coordinator page

--- frontend/src/pages/SpecificCoordinator.jsx
+++ frontend/src/pages/SpecificCoordinator.jsx
@@ -9,7 +9,7 @@ function SpecificCoordinator(){
 
     const {position} = useParams();
 
-    const coordinator = staffs.find((staff) => staff.position === position);
+    const coordinator = staffs.find((staff) => staff.position.trim() === position.trim());
 
     //also add something idk maybe for not found or somthing
     if (!coordinator) {
@@ -23,30 +23,48 @@ function SpecificCoordinator(){
         );
       }
 
-    return (
-        <>
-          <NavBar />
-          <div className="mx-5 my-5">
-            <Card>
-              <CardHeader color="lightBlue" className="text-center">
-                <img
-                  className="w-full h-64 object-cover"
-                  src={coordinator.urls}
-                  alt={coordinator.name}
-                />
-              </CardHeader>
-              <CardBody>
-                <Typography variant="h3" color="blue-gray" className="text-center">
-                  {coordinator.name}
-                </Typography>
-                <Typography variant="h5" color="gray" className="text-center">
-                  {coordinator.position}
-                </Typography>
-                <p className="text-center mt-4">{coordinator.Description}</p>
-              </CardBody>
-            </Card>
-          </div>
-        </>
-      );
+else {
+  return (
+    <>
+      <NavBar />
+      <div className="flex mx-5 my-5">
+        <div className="w-1/3 pr-5">
+          <img
+            className="w-full h-64 object-cover rounded-full"
+            src={coordinator.urls}
+            alt={coordinator.name}
+          />
+            <Typography variant="h3" color="blue-gray" className="text-center">
+                {coordinator.name}
+              </Typography>
+              <Typography variant="h5" color="gray" className="text-center">
+                {coordinator.position}
+              </Typography>
+              <Typography variant="h6" color="gray" className="text-center">
+                {coordinator.academic}
+              </Typography>
+              <div className="text-center mt-4">
+                <p>Contact:</p>
+                <p>Phone: {coordinator.phone}</p>
+                <p>Email: {coordinator.email}</p>
+              </div>
+        </div>
+        <div className="w-2/3">
+          <Card>
+            <CardBody>
+              <Typography variant="h2" color="green" className="text-center">
+                  {coordinator.sport}
+              </Typography>
+              <div className="text-left mt-4">
+                <p>About: </p>
+                <p>{coordinator.Description}</p>
+              </div>
+            </CardBody>
+          </Card>
+        </div>
+      </div>
+    </>
+  );  
+}
 }
 export default SpecificCoordinator;
