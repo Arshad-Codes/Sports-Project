175a0f60e299c751985a6b1632440f735e3bb22a
Infas_NM <87600043+infas01@users.noreply.github.com>
2024-03-14 20:04:00 +0530

    sports route

--- Backend/routes/sports.route.js
+++ Backend/routes/sports.route.js
@@ -1,16 +1,15 @@
 import express from 'express';
-import {
-  register,
-  login,
-  logout,
-  verify,
-} from '../controllers/student.controller.js';
+import { createsport } from '../controllers/sports.controller.js';
+import multer from 'multer';
+
+// router.post("/createsport",createsport);
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/logout', logout);
-router.get('/verify/:studentId/:uniqueString', verify);
+
+
+// imageurl for uploding image in clodinary
+// cloudinary media librayla you can see tha uploded picture
+router.post('/createsport', createsport);
 
 export default router;
