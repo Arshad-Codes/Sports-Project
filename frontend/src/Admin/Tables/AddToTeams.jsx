495814c41c430377de56aa068c601650c7933d1c
Nifras365 <nifrasmjm56@outlook.com>
2024-06-07 23:31:53 +0530

    Update AddToTeams.jsx

--- frontend/src/Admin/Tables/AddToTeams.jsx
+++ frontend/src/Admin/Tables/AddToTeams.jsx
@@ -243,6 +243,15 @@ import {
                         Student Name
                       </Typography>
                     </th>
+                    <th className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
+                      >
+                        Registration No
+                      </Typography>
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
@@ -261,6 +270,15 @@ import {
                             className="font-normal"
                           >
                             {student.firstName}
+                          </Typography>
+                        </td>
+                        <td className={classes}>
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {student.regNo}
                           </Typography>
                         </td>
                       </tr>
