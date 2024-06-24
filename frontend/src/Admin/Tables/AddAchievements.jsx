1ef599c8d3e9c1607941d27cde80da4d537f4038
Nifras365 <nifrasmjm56@outlook.com>
2024-06-24 10:26:06 +0530

    delete_achivements

--- frontend/src/Admin/Tables/AddAchievements.jsx
+++ frontend/src/Admin/Tables/AddAchievements.jsx
@@ -6,99 +6,213 @@ import { toast, ToastContainer } from "react-toastify";
 import { DeleteForever, Edit } from '@mui/icons-material';
 import axios from "axios";
 
-const AddAchievements = () =>{
+const AddAchievements = () => {
 
-    const [achievement, setAchievement] = useState({
+  const [achievement, setAchievement] = useState({
+    title: '',
+    description: '',
+    imgUrl: '',
+  });
+  const [achievementList, setAchievementList] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          'http://localhost:8800/api/achievement/'
+        );
+        setAchievementList(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching achievements:', error);
+      }
+    }
+    fetchData();
+  }, [achievementList]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(
+        'http://localhost:8800/api/achievement/create',
+        {
+          ...achievement,
+        },
+        { withCredentials: true }
+      );
+      toast.success('Achievement added successfully!', {
+        position: 'bottom-right',
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        style: {
+          borderRadius: '8px',
+          boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.1)',
+          padding: '16px',
+          fontSize: '16px',
+        },
+        iconTheme: {
+          primary: '#FFFFFF',
+          secondary: '#4CAF50',
+        },
+      });
+
+      //clear the fields
+      setAchievement({
         title: '',
         description: '',
         imgUrl: '',
+      });
+    } catch (error) {
+      console.error(error);
+      toast.error('Failed to add Achievement. Please try again.', {
+        position: 'bottom-right',
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        style: {
+          borderRadius: '8px',
+          boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.1)',
+          padding: '16px',
+          fontSize: '16px',
+        },
+        iconTheme: {
+          primary: '#FFFFFF',
+          secondary: '#FF5252',
+        },
+      });
+    }
+  };
+
+  const handleChange = (e) => {
+    setAchievement((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
     });
-    const [achievementList, setAnnouncementsList] = useState([]);
-    const [loading, setLoading] = useState(true);
+  };
 
-    useEffect(() => {
-      async function fetchData() {
-        try {
-          const response = await axios.get(
-            'http://localhost:8800/api/achievement/'
-          );
-          setAnnouncementsList(response.data);
-          setLoading(false);
-        } catch (error) {
-          console.error('Error fetching achievements:', error);
-        }
-      }
-      fetchData();
-    }, [achievementList]);
+  const handleEdit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.put(
+        `http://localhost:8800/api/achievement/${achievement.title}`,
+        {
+          ...achievement,
+        },
+        { withCredentials: true }
+      );
+      toast.success('Achievement added successfully!', {
+        position: 'bottom-right',
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        style: {
+          borderRadius: '8px',
+          boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.1)',
+          padding: '16px',
+          fontSize: '16px',
+        },
+        iconTheme: {
+          primary: '#FFFFFF',
+          secondary: '#4CAF50',
+        },
+      });
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-          await axios.post(
-            'http://localhost:8800/api/achievement/create',
-            {
-              ...achievement,
-            },
-            { withCredentials: true }
-          );
-          toast.success('Achievement added successfully!', {
-            position: 'bottom-right',
-            autoClose: 4000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            style: {
-              borderRadius: '8px',
-              boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.1)',
-              padding: '16px',
-              fontSize: '16px',
-            },
-            iconTheme: {
-              primary: '#FFFFFF',
-              secondary: '#4CAF50',
-            },
-          });
+      // Clear the fields
+      setAchievement({
+        title: '',
+        description: '',
+        imgUrl: '',
+      });
+    } catch (error) {
+      console.error(error);
+      toast.error('Failed to add Achievement. Please try again.', {
+        position: 'bottom-right',
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        style: {
+          borderRadius: '8px',
+          boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.1)',
+          padding: '16px',
+          fontSize: '16px',
+        },
+        iconTheme: {
+          primary: '#FFFFFF',
+          secondary: '#FF5252',
+        },
+      });
+    }
+  };
 
-          //clear the fields
-          setAchievement({
-            title: '',
-            description: '',
-            imgUrl: '',
-          });
-        } catch (error) {
-          console.error(error);
-          toast.error('Failed to add Achievement. Please try again.', {
-            position: 'bottom-right',
-            autoClose: 4000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            style: {
-              borderRadius: '8px',
-              boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.1)',
-              padding: '16px',
-              fontSize: '16px',
-            },
-            iconTheme: {
-              primary: '#FFFFFF',
-              secondary: '#FF5252',
-            },
-          });
-        }
-      };
+  const handleDelete = (achievement) => async () => {
+    try {
+      await axios.delete(
+        `http://localhost:8800/api/achievement/${achievement.title}`,
+        { withCredentials: true }
+      );
 
+      toast.success('Achievement deleted successfully', {
+        position: 'bottom-right',
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        style: {
+          borderRadius: '8px',
+          boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.1)',
+          padding: '16px',
+          fontSize: '16px',
+        },
+        iconTheme: {
+          primary: '#FFFFFF',
+          secondary: '#4CAF50',
+        },
+      });
+      setAchievementList(
+        achievementList.filter(
+          (_achievement) => _achievement._id !== achievement._id
+        )
+      );
+    } catch (error) {
+      console.error('Error deleting achievement', error);
+      toast.error('Failed, Check your internet connection and try again', {
+        position: 'bottom-right',
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        style: {
+          borderRadius: '8px',
+          boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.1)',
+          padding: '16px',
+          fontSize: '16px',
+        },
+        iconTheme: {
+          primary: '#FFFFFF',
+          secondary: '#FF5252',
+        },
+      });
+    }
+  };
 
-      const handleChange = (e) => {
-        setAchievement((prev) => {
-          return { ...prev, [e.target.name]: e.target.value };
-        });
-      };
-
-    return(
+  return (
     <div>
       <div className="flex items-center justify-center h-3/4 my-10 mx-5">
         <div className="py-10 px-14 w-full mx-auto border border-gray-300 border-t-0 shadow-lg rounded-lg">
@@ -188,7 +302,7 @@ const AddAchievements = () =>{
                     color="green"
                     size="sm"
                     className="!min-h-[30px] !py-1 !px-3 flex items-center justify-center"
-                   // onClick={handleEdit}
+                    onClick={handleEdit}
                   >
                     <Edit className="h-4 w-4" />
                   </Button>
@@ -196,7 +310,7 @@ const AddAchievements = () =>{
                     color="red"
                     size="sm"
                     className="!min-h-[30px] !py-1 !px-3 flex items-center justify-center"
-                    //onClick={handleDelete(announcement)}
+                    onClick={handleDelete(achievement)}
                   >
                     <DeleteForever className="h-4 w-4" />
                   </Button>
@@ -218,7 +332,7 @@ const AddAchievements = () =>{
         pauseOnHover
       />
     </div>
-    )
+  )
 }
 
 export default AddAchievements;
