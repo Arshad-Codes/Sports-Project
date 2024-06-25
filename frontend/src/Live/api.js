75c37787042e91445c190f77de2faae0b6348b23
Arshad-Codes <139666031+Arshad-Codes@users.noreply.github.com>
2024-06-26 00:46:38 +0530

    live missing
    live missing file

---
+++ frontend/src/Live/api.js
@@ -0,0 +1,26 @@
+//Auth token we will use to generate a meeting and connect to it
+export const authToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiJhOGQ4NTg2Mi0wZGRmLTQ4YWUtODc2MC0wYWRmMTJhMWUzY2YiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTcxOTEzMjQ3OSwiZXhwIjoxNzIxNzI0NDc5fQ.ERpxDv5OPp9gta9ap28ING6kTisR6XjaSko5IJRPVVw';
+
+// API call to create meeting
+export const createLiveStreaming = async () => {
+  const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
+    method: 'POST',
+    headers: {
+      authorization: `${authToken}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  //Destructuring the roomId from the response
+  const { roomId } = await res.json();
+  // console.log(roomId);
+  return roomId;
+};
+
+// export const fetchLiveMeetings = async () => {
+//   const res = await fetch('http://localhost:8800/api/live/getlive', {
+//     method: 'GET',
+//   });
+//   return await res.json();
+// };
