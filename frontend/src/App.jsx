8600520ef1efc0e94344a2acc4478c4e43d59662
Arshad-Codes <139666031+Arshad-Codes@users.noreply.github.com>
2024-06-25 23:38:37 +0530

    Revert "Merge branch 'Updated' into arshad"
    This reverts commit d07c37dbdf71ce1618b480faf24abe649d5dee31, reversing
    changes made to 6e41857b208077f63e84785e2973146ef1bac6c9.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,10 +13,7 @@ import StaffDashboard from './SportsCoordinator/StaffDashboard';
 import Achievement from './Achievementss/Achievement';
 import EmailPage from './Student/EmailPage';
 import ProfilePage from './Student/StudentProfile';
-import LiveMainPage from './Live/LiveMainPage';
-import Live from './Live/Live';
-import EditProfile from './Student/EditProfile';
-import SpeakerView from './Live/screens/SpeakerView';
+
 
 function App() {
   return (
@@ -39,12 +36,7 @@ function App() {
         <Route path="/staff/dashboard" element={<StaffDashboard />} />
         <Route path="/achievement" element={<Achievement />} />
         <Route path="/email" element={<EmailPage />} />
-        <Route path="/myprofile" element={<ProfilePage />} />
-        <Route path="/liveMain" element={<LiveMainPage />} />
-        <Route path="/live" element={<Live />} />
-        <Route path="/myprofile" element={<ProfilePage />} />
-        <Route path="/myprofile/edit" element={<EditProfile />} />
-        <Route path="/telecast" element={<SpeakerView />} />
+        <Route path="/myprofile" element={<ProfilePage/>}/>
       </Routes>
     </BrowserRouter>
   );
