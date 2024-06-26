bc06bc675fd78904c3bad3a045037bc8d8b0da0b
Infas_NM <87600043+infas01@users.noreply.github.com>
2024-06-27 03:47:16 +0530

    node frontend

--- Backend/routes/achievements.route.js
+++ Backend/routes/achievements.route.js
@@ -1,15 +1,21 @@
-import express from "express";
-import { getAchievements, getAchievement, createAchievement, updateAchievement, deleteAchievement,updateAchievements } from "../controllers/achievements.controller.js";
-import { verifyTokenAdmin } from "../middleware.js";
+import express from 'express';
+import {
+  getAchievements,
+  getAchievement,
+  createAchievement,
+  updateAchievement,
+  deleteAchievement,
+  updateAchievements,
+} from '../controllers/achievements.controller.js';
+import { verifyTokenAdmin } from '../middleware.js';
 
 const router = express.Router();
 
-router.get("/", getAchievements);
-router.get("/:id", getAchievement);
-router.post("/create", verifyTokenAdmin, createAchievement);
+router.get('/', getAchievements);
+router.get('/:id', getAchievement);
+router.post('/create', verifyTokenAdmin, createAchievement);
 //router.put("/:title",verifyTokenAdmin, updateAchievement);
-router.delete("/:title",verifyTokenAdmin, deleteAchievement);
-router.put("/:_id", updateAchievements);
-
+router.delete('/:title', verifyTokenAdmin, deleteAchievement);
+router.put('/:_id', updateAchievements);
 
 export default router;
